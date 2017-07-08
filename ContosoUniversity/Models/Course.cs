e4b4fbc2229ede7bde1b903a10a8fa95f6c7f5d0
rouz majlessi <rmajlessi@gmail.com>
2017-07-07 19:57:33 -0700

    add course class and add navaigation property of enrollment for course as did with student

---
+++ ContosoUniversity/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        // becaue Enrollment is a navagation property , meaning course and student class have a one-to-many relationshiip with enrollment we put the list here and for the student class but we do not have to p;ut the list for the navigation property class which is enrollment class
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -14,9 +14,17 @@ namespace ContosoUniversity.Models
 
     public class Enrollment
     {
+       // primary key , also we can simply use ID but for this example we used EnrollmentID unlike the student class file were we used Id both work this is for illustrative purpose.
+       // Although note that using ID without the name makes it easier to implement inheritance.
         public int EnrollmentID { get; set; }
         public int CourseId { get; set; }
+        
+        // the foreign key from the student class.
+        // Beacuse enrollment is associated with one student this object class(enrollment) can hold a single student vs in the student class were we have list and therfore we can hold multiple enrollments. Same goes for the CourseId in this class.
         public int StudentID { get; set; }
+
+        // Grade property is an enum declared at the top. ? declares the Grade property as nullable. Meaning the Grade has not been assigned as of yet. 
+        // If we dont put the ? mark the grade would not be nullabe and be assigned a value of 0 which we do not want.
         public Grade? Grade { get; set; }
 
         public Course Course { get; set; }
