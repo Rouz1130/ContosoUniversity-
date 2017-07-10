9a85552c1cd904784acb6d48ac6d41431f1fe435
rouz majlessi <rmajlessi@gmail.com>
2017-07-10 16:38:24 -0700

    update EF to add datatype and display format for validation on character limit

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -9,9 +9,15 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int ID { get; set; }
+        //StringLength attribute most amount of characters allowed for name 
+        [StringLength(50)]
         public string LastName { get; set; }
+        [StringLength(50, ErrorMessage ="First name cannot be longer than 50 characters.")]
         public string FirstMidName { get; set; }
-        [DataType(DataType.Date)]
+        // DataType attribute used to specify a data type more specific and db intrinsic type.
+        // DataType provides various types e.g Date, Time, PhoneNumber, Currency, EmailAddress etc.
+        // DataType.Date does not specify date fromat it has a default, That is why we use the DisplayFormat attribute to specify the date format to our liking.
+        [DataType(DataType.Date)]                         // Below specifies for value in textbox for editing.
         [DisplayFormat(DataFormatString ="{0:yyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
 
