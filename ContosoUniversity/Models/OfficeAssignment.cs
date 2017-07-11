dd238cb4f4670443e972e0dda877b62946603ba8
rouz majlessi <rmajlessi@gmail.com>
2017-07-10 17:16:58 -0700

    add attriclear

---
+++ ContosoUniversity/Models/OfficeAssignment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public class OfficeAssignment
+    {
+
+        // Key attribute 
+        [Key]
+        public int InstructorID { get; set; }
+        [StringLength(50)]
+        [Display(Name="Office Location")]
+        public string Location { get; set; }
+
+
+        // both Insturctor and officeAssinment entities aer nullable because instructor might not have office space
+        // and therfore cannot exist without and instructor or officeassingment
+        public Instructor Instructor { get;set }
+    }
+}
