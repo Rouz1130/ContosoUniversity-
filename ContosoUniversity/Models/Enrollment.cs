a224a4ebb0a27df338d601dc24985ea70c78b03d
rouz majlessi <rmajlessi@gmail.com>
2017-07-10 17:29:07 -0700

    add displayFormat attribute in enromodel/enrollment entity

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -22,6 +24,7 @@ namespace ContosoUniversity.Models
         // the foreign key from the student class.
         // Beacuse enrollment is associated with one student this object class(enrollment) can hold a single student vs in the student class were we have list and therfore we can hold multiple enrollments. Same goes for the CourseId in this class.
         public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText ="No grade")]
 
         // Grade property is an enum declared at the top. ? declares the Grade property as nullable. Meaning the Grade has not been assigned as of yet. 
         // If we dont put the ? mark the grade would not be nullabe and be assigned a value of 0 which we do not want.
